bb0f4007f2648915354aefaf7ba8829d1a8b94c5
Dustin McGuire <dustinmcguire@MacBook-Air.attlocal.net>
2020-03-02 13:26:00 -0600

    swift practice

--- SwiftPractice.playground/Contents.swift
+++ SwiftPractice.playground/Contents.swift
@@ -106,3 +106,18 @@ thirdScore > fourthScore
 thirdScore < fourthScore
 
 thirdScore <= fourthScore
+
+/*
+ Conditions using if else statments to make blackjack
+ */
+
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 2 {
+    print("Aces A lucky!")
+} else if firstCard + secondCard == 21{
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
