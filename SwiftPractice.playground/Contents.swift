72a43d65903848a1d66afb38600459ab6334dac3
Dustin McGuire <dustinmcguire@MacBook-Air.attlocal.net>
2020-03-03 15:35:11 -0600

    swift practice

--- SwiftPractice.playground/Contents.swift
+++ SwiftPractice.playground/Contents.swift
@@ -219,3 +219,24 @@ while  number <= 20 {
 }
 
 print ("Ready or not, here I come!")
+
+/*
+ Repeat loop
+ */
+
+var number = 1
+
+repeat {
+    print(number)
+    number += 1
+} while number <= 20
+
+print("Ready or not, here I come!")
+
+while false {
+    print("This is false")
+}
+
+repeat {
+    print("This is false")
+} while false
