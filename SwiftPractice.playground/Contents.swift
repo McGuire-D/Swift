1287bcb51a110bcf6a72c1c22d9646e0822e3fbe
Dustin McGuire <dustinmcguire@MacBook-Air.attlocal.net>
2020-03-03 15:50:04 -0600

    swift practice

--- SwiftPractice.playground/Contents.swift
+++ SwiftPractice.playground/Contents.swift
@@ -257,3 +257,19 @@ while countDown >= 0 {
 }
 
 print("Blast Off!")
+
+/*
+ exiting multiple loops
+ */
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+        
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
