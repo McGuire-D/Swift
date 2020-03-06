9ff08573f321cfab8376f095baa8cd84248e94c1
Dustin McGuire <dustinmcguire@MacBook-Air.attlocal.net>
2020-03-06 15:49:01 -0600

    swift practice

--- SwiftPractice.playground/Contents.swift
+++ SwiftPractice.playground/Contents.swift
@@ -506,3 +506,19 @@ let driving2 = { (place: String) ->
 let message = driving2("London")
 
 print(message)
+
+/*
+ Closures as parameters
+ */
+
+let driving3 = {
+    print("I'm driving in my car")
+}
+
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travel(action: driving3)
