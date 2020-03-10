1623725fcd15fe0f6a4ec0f925fef133c74fca51
Dustin McGuire <dustinmcguire@MacBook-Air.local>
2020-03-10 15:34:14 -0500

    swift practice

--- SwiftPractice.playground/Contents.swift
+++ SwiftPractice.playground/Contents.swift
@@ -537,5 +537,18 @@ travel1 {
     print("I'm driving in my car")
 }
 
+/*
+ Using closures as parameters when they accept parameters
+ */
+
+func travel(action: (String) -> Void) {
+    print("I'm getting ready to go.")
+    action("London")
+    print("I arrived!")
+}
+
+travel { (place: String) in
+    print("I'm going to \(place) in my car")
+}
 
 
