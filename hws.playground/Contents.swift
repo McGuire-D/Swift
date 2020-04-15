acdc6be7b2c52d3e914ce24a99cd7d3773ee0359
Dustin McGuire <mcguire.dustin@att.net>
2020-04-14 20:32:01 -0500

    Swift practice

---
+++ hws.playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+/*
+Day 1 Hacking with swift
+
+Lesson 1: Variables
+ */
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+/*
+ Lesson 2: Strings and integers
+ */
+
+var age = 38
+
+var chocolate = "Jello"
+
+var populatiion = 8_000_000
+
+/*
+Lesson 3: Multi-line strings
+ */
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 =  """
+This goes \
+over multiple \
+lines
+"""
+
+/*
+ Lesson 4: Doubles and booleans
+ */
+
+var pi = 3.141
+
+var awesome = true
+
+/*
+ Lesson 5: String interpolation
+ */
+
+var score = 85
+
+var str3 = "Your score was \(score)"
+
+var results = "The test results are: \(str)"
+
+/*
+ Lesson 6: Constants
+ */
+
