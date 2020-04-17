d7a76d8c85ff6f0cfa6c855b38a47b0cb51b2170
Dustin McGuire <mcguire.dustin@att.net>
2020-04-16 20:32:14 -0500

    practice

---
+++ hws2.playground/Contents.swift
@@ -0,0 +1,13 @@
+/*
+Lesson 19: Operator overloading
+ */
+
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+let fakers = "Fakers gonna"
+let actions = fakers + "fake"
+
+let firstHalf = ["John","Ringo"]
+let secondHalf = ["George","Ringo"]
+let beatles = firstHalf+secondHalf
