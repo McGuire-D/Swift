d1319f4c3781e57ad96db06bdf351636525fd445
Dustin McGuire <mcguire.dustin@att.net>
2020-04-20 18:20:37 -0500

    Practice swift

--- hws2.playground/Contents.swift
+++ hws2.playground/Contents.swift
@@ -226,3 +226,21 @@ while true{
         break
     }
 }
+
+/*
+Lesson 34: Writing functions
+ */
+
+func printHelp() {
+    let message = """
+Welcome to MyApp!
+
+Run this app inside a directory of images and
+MyApp will resize them all into thumbnails
+"""
+
+    print(message)
+}
+
+printHelp()
+
