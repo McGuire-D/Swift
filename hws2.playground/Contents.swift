5b6101a5f8f6c4179b6b64a96644ff4c1d9c7fd9
Dustin McGuire <mcguire.dustin@att.net>
2020-04-20 19:28:29 -0500

    Practice swift

--- hws2.playground/Contents.swift
+++ hws2.playground/Contents.swift
@@ -346,3 +346,27 @@ func checkPassword(_ password: String) throws -> Bool {
 }
 
 
+/*
+Lesson 42: Running throwing functions
+ */
+
+
+enum PasswordError1: Error {
+    case obvious
+}
+
+
+func checkPassword1(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    
+    return true
+}
+
+do {
+    try checkPassword("password")
+    print("That passoword is good")
+} catch {
+    print("You can't use that password.")
+}
