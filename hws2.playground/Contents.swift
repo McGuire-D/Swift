56bb20c9c500fa3c91d9a286373a92d07a99311d
Dustin McGuire <mcguire.dustin@att.net>
2020-04-22 12:07:52 -0500

    Swift practive

--- hws2.playground/Contents.swift
+++ hws2.playground/Contents.swift
@@ -493,3 +493,41 @@ func travel(action: (String) -> String) {
 travel { (place: String) -> String in
     return "I'm going to \(place) in my car"
 }
+
+
+
+/*
+Lesson 51: Shorthand parameter names
+ */
+
+func travel1(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+
+travel { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+
+travel { place -> String in
+    return "I'm going to \(place) in my car"
+}
+
+
+travel { place in
+    return "I'm going to \(place) in my car"
+}
+
+
+travel { place in
+    "I'm going to \(place) in my car"
+}
+
+
+travel {
+    "I'm going to \($0) in my car"
+}
