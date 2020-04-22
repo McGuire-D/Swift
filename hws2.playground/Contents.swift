506387a99cf6ec335974834f759c1d7eb39ad2c0
Dustin McGuire <mcguire.dustin@att.net>
2020-04-22 11:59:54 -0500

    Swift practive

--- hws2.playground/Contents.swift
+++ hws2.playground/Contents.swift
@@ -481,3 +481,15 @@ travel { (place: String) in
 /*
 Lesson 50: Using closures as parameters when they return values
  */
+
+
+func travel(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+travel { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
