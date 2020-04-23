1989f209a3c4b24bb6da8ca19f8734ac4697cb46
Dustin McGuire <mcguire.dustin@att.net>
2020-04-23 18:12:22 -0500

    Swift practice

--- hws3.playground/Contents.swift
+++ hws3.playground/Contents.swift
@@ -60,3 +60,21 @@ struct Progress {
         }
     }
 }
+
+
+/*
+Lesson 58: Methods
+ */
+
+
+struct City {
+    var population: Int
+
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+
+let london = City(population: 9_000_000)
+london.collectTaxes()
