cc0e9535c49cc7e1abe6328909864ca69e601eca
Dustin McGuire <mcguire.dustin@att.net>
2020-04-23 17:57:48 -0500

    new folder

--- hws3.playground/Contents.swift
+++ hws3.playground/Contents.swift
@@ -1,2 +1,14 @@
 import UIKit
 
+/*
+Lesson 55: Creating your own structs
+ */
+
+struct Sport {
+    var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+tennis.name = "Lawn tennis"
