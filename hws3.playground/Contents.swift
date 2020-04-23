32573019ccf3014bad4837357ba22d42b2389cfb
Dustin McGuire <mcguire.dustin@att.net>
2020-04-23 18:04:20 -0500

    new folder

--- hws3.playground/Contents.swift
+++ hws3.playground/Contents.swift
@@ -12,3 +12,28 @@ var tennis = Sport(name: "Tennis")
 print(tennis.name)
 
 tennis.name = "Lawn tennis"
+
+/*
+Lesson 56: Computed properties
+ */
+
+struct Sport1 {
+    var name: String
+}
+
+
+struct Sport2 {
+    var name: String
+    var isOlympicSport: Bool
+
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let chessBoxing = Sport2(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
