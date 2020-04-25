2552c12c13e20e17e565655d5b2f623a53eaa7d0
Dustin McGuire <mcguire.dustin@att.net>
2020-04-25 11:29:03 -0500

    swift practice

--- hws3.playground/Contents.swift
+++ hws3.playground/Contents.swift
@@ -289,3 +289,30 @@ class Dog {
 }
 
 let poppy = Dog(name: "Poppy", breed: "Poodle")
+
+
+
+/*
+Lesson 68: Class inheritance
+ */
+
+class Dog1 {
+    var name: String
+    var breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+class Poodle: Dog1 {
+    init(name: String) {
+        super.init(name: name, breed: "Poodle")
+    }
+}
+
+
+/*
+Lesson 69: Overriding methods
+ */
